158316afa509e05fbb00fd81f6ed13f0a67761e8
Pradyum-02 <pradyumofc13@gmail.com>
2026-07-28 04:37:44 +0530

    Fix register user email check

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -40,6 +40,8 @@ if (existingEmployee) {
     });
 }
 
+const existingUser = await User.findOne({ email });
+
         if (existingUser) {
             return res.status(400).json({
                 message: "Email already registered."
